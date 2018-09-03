de17ae5df971cc2de601cccf8ceea44b3f6b1594
mahirjain25 <mahirjain25@gmail.com>
2018-09-03 10:08:40 +0530

    Added Drivers table in script.

--- script.sql
+++ script.sql
@@ -41,3 +41,50 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+CREATE TABLE Drivers( DriverID int AUTO_INCREMENT PRIMARY KEY, 
+	Name varchar(20),
+	Tot_Points int(3),
+	Cost decimal,
+	CONSTRAINT check_cost CHECK (Cost BETWEEN 0 AND 20));
+
+ALTER TABLE Drivers
+ADD Week_Points int(3) DEFAULT 0;
+
+ALTER TABLE Drivers
+MODIFY Cost float;
+
+DELIMITER $$
+CREATE TRIGGER test_cost_insert BEFORE INSERT ON Drivers
+FOR EACH ROW
+BEGIN
+	IF NEW.Cost NOT BETWEEN 0 AND 20 THEN
+		SIGNAL SQLSTATE '12345'
+		SET MESSAGE_TEXT = 'Driver Cost must be between 0 and 20.';
+	END IF;
+END$$
+DELIMITER ;
+
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Lewis Hamilton",314,30.6);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Sebastian Vettel",285,28.7);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Kimi Raikkonen",252,19.3);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Daniel Ricciardo",207,26.0);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Valtteri Bottas",186,26.3);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Max Verstappen",153,21.9); 
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Fernando Alonso",107,14.1);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Stoffel Vandoorne",40,6.6);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Nico Hulkenburg",84,12.5);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Carlos Sainz",111,12.1);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Esteban Ocon",96,10.8);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Sergio Perez",64,9.9);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Kevin Magnussen",100,7.5);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Romain Grosjean",34,5.8);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Charles LeClerc",77,6.2);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Marcus Ericsson",26,5.5);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Pierre Gasly",48,7.0);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Brendon Hartley",39,6.1);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Sergey Sirotkin",21,5.1);
+INSERT INTO Drivers (Name,Tot_Points,Cost) VALUES("Lance Stroll",30,5.3);
+
+
+
