d7e95587c5775a628f2ae2ae36f2818adb184d36
mahirjain25 <mahirjain25@gmail.com>
2018-09-10 15:51:52 +0530

    Tested HTML rendering.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var things = require('./things.js');
-
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'views')));
 app.use('/',things);
 
 app.listen(3000);
--- things.js
+++ things.js
@@ -10,4 +10,7 @@ router.all('/learn',function(req,res){
 	res.send("Using the ALL HTTP methods feature.");
 });
 
+router.get('/template', function(request, response) {
+    response.sendFile('views/main.html', { root: __dirname }); //Since we have configured to use public folder for serving static files. We don't need to append public to the html file path.
+});
 module.exports = router;
