e32facc98d2e73888ce13fa9a7dd22623642cc24
suraj44 <suraj1998@gmail.com>
2018-09-18 13:05:51 +0530

    added a few functions to driver, started using Closure coding style

--- drivers/README.md
+++ drivers/README.md
@@ -11,4 +11,9 @@ Need to figure out how to write the getAllDrivers() function correctly.
 Figured out how to deal with the scoping problem.
 refer [driver-model.js](./driver-model.js) for detailed explanation. 
 Need to read more about callbacks and why they are needed. But I Fixed the problem referring to [this link](https://stackoverflow.com/questions/31875621/how-to-properly-return-a-result-from-mysql-with-node).
-TODO: Try exporting the results of getAllDrivers() to the controller file (which is yet to be created). 
+TODO for MJ: Try exporting the results of getAllDrivers() to the controller file (which is yet to be created).
+
+### 18/9/2018
+Started writing the ```updateDriverWeeklyScore()``` function. Learnt how to 'mark' the datatypes of the function parameters for the compiler. Not only datatypes of parameters, but things like if the function is a constructor or something similar can be marked as well. Refer [THIS](https://github.com/google/closure-compiler/wiki/Annotating-JavaScript-for-the-Closure-Compiler) for more details of the same. 
+Refer [THIS](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures) for the different datatypes JavasScript has
+Completed ```updateDriverWeeklyScore()``` and ```getDriversSortedByTotalPoints```.
--- drivers/driver-model.js
+++ drivers/driver-model.js
@@ -1,3 +1,6 @@
+/**
+ * To test this file, execute node driver-model.js in the teriminal
+ */
 const mysql = require("mysql")
 
 const sql = mysql.createConnection({
@@ -22,6 +25,9 @@ sql.connect(function (err) {
 // If you simply tried to return result, the problem is of the scope of the variable
 // So in the return statement of this function, I'm actually calling the callback() function which is defined at line #47
 // and that function passes the value of result to the global variable L.
+/**
+ * Function that fetches all drivers, unordered
+ */
 function getAllDrivers(callback) {
     sql.query('SELECT * from Drivers', function(err, results){
         if (err) {
@@ -32,7 +38,40 @@ function getAllDrivers(callback) {
     })
 }
 
+/**
+ * Function that fetches all drivers from the database, ordered by total points
+ */
+function getDriversSortedByTotalPoints(callback) {
+    sql.query('SELECT * from Drivers ORDER BY Tot_Points', function(err, results){
+        if (err) {
+            throw err;
+        }
 
+        return callback(results);
+    })
+
+}
+
+
+// Since JavaScript is a loosely typed language, you can't specify what the datatype of a function should be.
+// However, you can 'mark' the datatype of the parameters for the compiler. The format for doing so is: (refer to the readme for more reference)
+// Number follows the IEEE 754 precision standard and hence, includes floats as well. There's no specific type for int.
+// See, the function works even without a callback, but I added it because we would want to return to the frontend that the operation has been completed 
+// without having to deal with that asychronous issue.
+/**
+ * @param {Number} driverID The ID of the driver whose score needs to be updated
+ * @param {Number} newScore The new score of the corresponding driver
+ * @return {void} No return value222
+ */
+function updateDriverWeeklyScore(driverID, newScore, callback){
+    sql.query('UPDATE Drivers SET Week_Points = ? WHERE DriverID = ?', [newScore, driverID], function (err) {
+        if(err) {
+            throw err;
+        }
+        return callback(err);
+
+    })
+}
 
 
 var Driver = {
@@ -45,10 +84,23 @@ var Driver = {
 
 var L;
 
-getAllDrivers(function(result){
-    //console.log(result);
-    L = result;
-    console.log(L[0]);
-})
+// Uncomment whichever function call you want, to test the corresponding function.
+
+// updateDriverWeeklyScore(1, 20.2, function(error) {
+//     if(error) {
+//         throw error
+//     }
+//     console.log("Weekly score has been updated.");
+// })
+
+// getAllDrivers(function(result){
+//     //console.log(result);
+//     L = result;
+//     console.log(L[0]);
+// })
+
+// getDriversSortedByTotalPoints(function(result) {
+//     console.log(result);
+// })
 
 module.exports = Driver;
